667d0c60a785e073582eb834ac0144f90a162f97
PedroRM <preyesmedina@gmail.com>
2021-10-23 22:44:56 -0500

    Crear function check_events and update_screen for simplify while loop in alien_invasion

--- game_functions.py
+++ game_functions.py
@@ -7,3 +7,11 @@ def check_events():
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit()
+
+def update_screen(ai.settings,screen,ship):
+"""Update images on the screen and flip to the new screen"""
+        # Redraw the screen during each pass through the loop
+        screen.fill(ai_settings.bg_color)
+        ship.blitme()
+        # Make the most recently drawn screen visible
+        pygame.display.flip()
