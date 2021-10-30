c078daf00eabe11add9b0e68910a1f6f3a43b833
PedroRM <preyesmedina@gmail.com>
2021-10-29 20:25:29 -0500

    Add alien

--- game_functions.py
+++ game_functions.py
@@ -9,7 +9,13 @@ def check_keydown_events(event,ai_settings,screen,ship,bullets):
     elif event.key == pygame.K_LEFT:
         ship.moving_left = True
     elif event.key == pygame.K_SPACE:
-        #Create anew bullet and add it to the bullets group
+        fire_bullet(ai_settings,screen,ship,bullets)
+    elif event.key == pygame.k_q:
+        sys.exit()
+def fire_bullet(ai_settings,screen,ship,bullets):
+    """Fire a bullet if limit not reached yet"""
+    #Create anew bullet and add it to the bullets group
+    if len(bullets) < ai_settings.bullets_allowed:
         new_bullet=Bullet(ai_settings,screen,ship)
         bullets.add(new_bullet)
 
@@ -37,10 +43,20 @@ def check_events(ai_settings,screen,ship,bullets):
 
 
 
-def update_screen(ai_settings,screen,ship,bullets):
+def update_screen(ai_settings,screen,ship,alien,bullets):
 
     screen.fill(ai_settings.bg_color)
     for bullet in bullets.sprites():
         bullet.draw_bullet()
     ship.blitme()
+    alien.blitme()
     pygame.display.flip()
+
+def update_bullets(bullets):
+    """"Update position of bullets and get rid of old bullets"""
+    #Update bullet positions
+    bullets.update()
+    # Get rid of bullets that have disappeared
+    for bullet in bullets.copy():
+        if bullet.rect.bottom <= 0:
+            bullets.remove(bullet)
