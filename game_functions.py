7e2b258385ce12fb91c25434ce11a27ca119977c
PedroRM <preyesmedina@gmail.com>
2021-10-31 22:51:59 -0600

    displaying the number of ships

--- game_functions.py
+++ game_functions.py
@@ -4,11 +4,13 @@ import pygame
 from bullet import Bullet
 from alien import Alien
 
-def ship_hit(ai_settings,stats,screen,ship,aliens,bullets):
+def ship_hit(ai_settings,screen,stats,sb,ship,aliens,bullets):
     """Respond tho ship being hit by alien"""
     if stats.ships_left>0:
         #Decrement ships_left
         stats.ships_left -=1
+        #Update scoreboard
+        sb.prep_ships()
         #Empty the list of aliens and bullets
         aliens.empty()
         bullets.empty()
@@ -117,6 +119,7 @@ def check_play_button(ai_settings,screen,stats,sb,play_button,ship,aliens,bullet
         sb.prep_score()
         sb.prep_high_score()
         sb.prep_level()
+        sb.prep_ships()
         #Empty the list of aliens and bullets
         aliens.empty()
         bullets.empty()
@@ -171,25 +174,25 @@ def check_bullet_alien_collisions(ai_settings,screen,stats,sb,ship,aliens,bullet
         sb.prep_level()
         create_fleet(ai_settings,screen,ship,aliens)
 
-def check_aliens_bottom(ai_settings,stats,screen,ship,aliens,bullets):
+def check_aliens_bottom(ai_settings,screen,stats,sb,ship,aliens,bullets):
     """Check if any aliens have reached the bottom of the screen"""
     screen_rect = screen.get_rect()
     for alien in aliens.sprites():
         if alien.rect.bottom >= screen_rect.bottom:
             #Treat this the same as if the ship got hit
-            ship_hit(ai_settings,stats,screen,ship,aliens,bullets)
+            ship_hit(ai_settings,screen,stats,sb,ship,aliens,bullets)
             break
 
-def update_aliens(ai_settings,stats,screen,ship,aliens,bullets):
+def update_aliens(ai_settings,screen,stats,sb,ship,aliens,bullets):
     """Update the positions of all aliens int the fleet"""
     check_fleet_edges(ai_settings,aliens)
     aliens.update()
     #Look for alien-ship collisions
     if pygame.sprite.spritecollideany(ship,aliens):
-        ship_hit(ai_settings,stats,screen,ship,aliens,bullets)
+        ship_hit(ai_settings,screen,stats,sb,ship,aliens,bullets)
         print("Ship hit!!!")
     #Loook for aliens hitting the bottom of the screen
-    check_aliens_bottom(ai_settings,stats,screen,ship,aliens,bullets)
+    check_aliens_bottom(ai_settings,screen,stats,sb,ship,aliens,bullets)
 
 
 def check_fleet_edges(ai_settings,aliens):
